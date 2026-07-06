89dcccf15f5cb8f2795241bda4542304605dec33
russhpng <rphotos4270@gmail.com>
2026-07-06 15:59:33 -0400

    Delete photos.js

--- photos.js
+++
@@ -1,8 +0,0 @@
-const photos = [
-  { file: "Ellicottville-2021_Trees.jpg", album: "ellicottville-2021" },
-  { file: "Hilton-Head-Island-2021_Beach.jpg", album: "hilton-head-island-2021" },
-  { file: "Hilton-Head-Island-2021_Clouds.jpg", album: "hilton-head-island-2021" },
-  { file: "Hilton-Head-Island-2021_Fire.jpg", album: "hilton-head-island-2021" },
-  { file: "Vacherie-2022_Front.jpg", album: "vacherie-2022" },
-  { file: "Vacherie-2022_Tunnel.jpg", album: "vacherie-2022" },
-];
